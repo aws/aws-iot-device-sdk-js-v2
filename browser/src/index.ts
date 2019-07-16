458c1ab053940a5c6962fa9c3e9ddac4106c467a
Justin Boswell <boswej@amazon.com>
2019-07-16 14:59:35 -0700

    Added async-mqtt, io shim

--- browser/src/index.ts
+++ browser/src/index.ts
@@ -11,3 +11,11 @@
 * express or implied. See the License for the specific language governing
 * permissions and limitations under the License.
 */
+
+import * as io from "./io";
+import * as mqtt from "./mqtt";
+
+export {
+    io,
+    mqtt,
+}
--- browser/src/mqtt.ts
+++ browser/src/mqtt.ts
@@ -12,6 +12,9 @@
 * permissions and limitations under the License.
 */
 
+import * as MQTT from "async-mqtt";
+import * as io from "./io";
+
 export interface ConnectionConfig {
     client_id: string;
     host_name: string;
@@ -40,7 +43,7 @@ export class AwsIotMqttConnectionConfigBuilder {
             clean_session: false,
             keep_alive: undefined,
             will: undefined,
-            username: '?SDK=NodeJSv2&Version=0.2.0',
+            username: '?SDK=JSv2&Version=0.1.0',
             password: undefined,
             alpn_list: 'x-amzn-mqtt-ca',
         };
@@ -104,5 +107,212 @@ export class AwsIotMqttConnectionConfigBuilder {
         }
 
         return this.params;
+    }
+}
+
+export class Client {
+    private bootstrap: io.ClientBootstrap;
+    private tls_ctx?: io.ClientTlsContext;
+
+    constructor(bootstrap: io.ClientBootstrap, tls_ctx?: io.ClientTlsContext) {
+        this.bootstrap = bootstrap;
+        this.tls_ctx = tls_ctx;
+    }
+
+    new_connection(config: ConnectionConfig) {
+        return new Connection(this, config);
+    }
+
+    native_handle(): any {
+        return undefined;
+    }
+}
+
+export enum QoS {
+    AtMostOnce = 0,
+    AtLeastOnce = 1,
+    ExactlyOnce = 2,
+}
+
+export interface MqttRequest {
+    packet_id: number;
+}
+
+export interface MqttSubscribeRequest extends MqttRequest {
+    topic: string;
+    qos: QoS;
+    error_code: number;
+}
+
+type Payload = string | Object | DataView;
+
+export class Connection {
+    readonly client: Client;
+    private encoder: TextEncoder;
+    private config: ConnectionConfig
+
+    constructor(client: Client, config: ConnectionConfig, on_connection_interrupted?: (error_code: number) => void, on_connection_resumed?: (return_code: number, session_present: boolean) => void) {
+        this.client = client;
+        this.config = config;
+        this.encoder = new TextEncoder();
+    }
+
+    close() {
+        
+    }
+
+    async connect() {
+        return new Promise<boolean>((resolve, reject) => {
+
+            function on_connect(error_code: number, return_code: number, session_present: boolean) {
+                console.log("on_connect ec:", error_code);
+                if (error_code == 0 && return_code == 0) {
+                    resolve(session_present);
+                } else if (error_code != 0) {
+                    reject("Failed to connect: " + io.error_code_to_string(error_code));
+                } else {
+                    reject("Server rejected connection.");
+                }
+            }
+
+            try {
+                /*  crt_native.mqtt_client_connection_connect(
+                    this.native_handle(),
+                    this.config.client_id,
+                    this.config.host_name,
+                    this.config.port,
+                    this.config.tls_ctx ? this.config.tls_ctx.native_handle() : null,
+                    this.config.connect_timeout,
+                    this.config.keep_alive,
+                    this.config.timeout,
+                    this.config.will,
+                    this.config.username,
+                    this.config.password,
+                    this.config.use_websocket,
+                    this.config.clean_session,
+                    on_connect,
+                );*/
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    async reconnect() {
+        return new Promise<boolean>((resolve, reject) => {
+
+            function on_connect(error_code: number, return_code: number, session_present: boolean) {
+                if (error_code == 0 && return_code == 0) {
+                    resolve(session_present);
+                } else if (error_code != 0) {
+                    reject("Failed to connect: " + io.error_code_to_string(error_code));
+                } else {
+                    reject("Server rejected connection.");
+                }
+            }
+
+            try {
+                //crt_native.mqtt_client_connection_reconnect(this.native_handle(), on_connect);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    async publish(topic: string, payload: Payload, qos: QoS, retain: boolean = false) {
+        return new Promise<MqttRequest>((resolve, reject) => {
+
+            let payload_data: DataView | undefined = undefined;
+            if (payload instanceof DataView) {
+                // If payload is already dataview, just use it
+                payload_data = payload;
+            } else {
+                if (typeof payload === 'object') {
+                    // Convert payload to JSON string, next if block will turn it into a DataView.
+                    payload = JSON.stringify(payload);
+                }
+
+                if (typeof payload === 'string') {
+                    // Encode the string as UTF-8
+                    payload_data = new DataView(this.encoder.encode(payload).buffer);
+                } else {
+                    return reject(new TypeError("payload parameter must be a string, object, or DataView."));
+                }
+            }
+
+            function on_publish(packet_id: number, error_code: number) {
+                if (error_code == 0) {
+                    resolve({ packet_id });
+                } else {
+                    reject("Failed to publish: " + io.error_code_to_string(error_code));
+                }
+            }
+
+            try {
+                //crt_native.mqtt_client_connection_publish(this.native_handle(), topic, payload_data, qos, retain, on_publish);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    async subscribe(topic: string, qos: QoS, on_message: (topic: string, payload: ArrayBuffer) => void) {
+        return new Promise<MqttSubscribeRequest>((resolve, reject) => {
+
+            function on_suback(packet_id: number, topic: string, qos: QoS, error_code: number) {
+                if (error_code == 0) {
+                    resolve({ packet_id, topic, qos, error_code });
+                } else {
+                    reject("Failed to subscribe: " + io.error_code_to_string(error_code));
+                }
+            }
+
+            try {
+                //crt_native.mqtt_client_connection_subscribe(this.native_handle(), topic, qos, on_message, on_suback);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    async unsubscribe(topic: string) {
+        return new Promise<MqttRequest>((resolve, reject) => {
+
+            function on_unsuback(packet_id: number, error_code: number) {
+                if (error_code == 0) {
+                    resolve({ packet_id });
+                } else {
+                    reject("Failed to unsubscribe: " + io.error_code_to_string(error_code));
+                }
+            }
+
+            try {
+                //crt_native.mqtt_client_connection_unsubscribe(this.native_handle(), topic, on_unsuback);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    async disconnect() {
+        return new Promise<void>((resolve, reject) => {
+
+            function on_disconnect() {
+                resolve();
+            }
+
+            try {
+                // crt_native.mqtt_client_connection_disconnect(
+                //     this.native_handle(),
+                //     on_disconnect,
+                // );
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    native_handle(): any {
+        return undefined;
     }
 }
