95daf34d2393fb7e6b3105081d37f3acbedfd4dd
Bret Ambrose <bretambrose@gmail.com>
2021-10-14 11:11:36 -0700

    New javascript-only version of pubsub.  Shared command line argument processing and connection creation

--- samples/node/pub_sub_js/index.js
+++ samples/node/pub_sub_js/index.js
@@ -3,27 +3,22 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-import { mqtt } from 'aws-iot-device-sdk-v2';
-import { TextDecoder } from 'util';
-
-type Args = { [index: string]: any };
-
+const iotsdk = require('aws-iot-device-sdk-v2');
+const mqtt = iotsdk.mqtt;
+const TextDecoder = require('util').TextDecoder;
 const yargs = require('yargs');
+const common_args = require('../../util/cli_args');
 
-// The relative path is '../../util/cli_args' from here, but the compiled javascript file gets put one level
-// deeper inside the 'dist' folder
-const common_args = require('../../../util/cli_args');
-
-yargs.command('*', false, (yargs: any) => {
+yargs.command('*', false, (yargs) => {
     common_args.add_connection_establishment_arguments(yargs);
     common_args.add_pub_sub_arguments(yargs)
 }, main).parse();
 
-async function execute_session(connection: mqtt.MqttClientConnection, argv: Args) {
+async function execute_session(connection, argv) {
     return new Promise(async (resolve, reject) => {
         try {
             const decoder = new TextDecoder('utf8');
-            const on_publish = async (topic: string, payload: ArrayBuffer, dup: boolean, qos: mqtt.QoS, retain: boolean) => {
+            const on_publish = async (topic, payload, dup, qos, retain) => {
                 const json = decoder.decode(payload);
                 console.log(`Publish received. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`);
                 console.log(json);
@@ -53,7 +48,7 @@ async function execute_session(connection: mqtt.MqttClientConnection, argv: Args
     });
 }
 
-async function main(argv: Args) {
+async function main(argv) {
     common_args.apply_sample_arguments(argv);
 
     const connection = common_args.build_connection_from_cli_args(argv);
@@ -63,9 +58,9 @@ async function main(argv: Args) {
     //    pinning the libuv event loop while the connection is active or potentially active.
     const timer = setInterval(() => {}, 60 * 1000);
 
-    await connection.connect()
-    await execute_session(connection, argv)
-    await connection.disconnect()
+    await connection.connect();
+    await execute_session(connection, argv);
+    await connection.disconnect();
 
     // Allow node to die if the promise above resolved
     clearTimeout(timer);
