0f2c26153009ea5b1f41e4d65e2e203c6600411d
Noah Beard <ncbeard@amazon.com>
2022-03-21 10:58:06 -0400

    Fixed codebuild tests

--- codebuild/samples/connect-linux.sh
+++ codebuild/samples/connect-linux.sh
@@ -4,16 +4,20 @@ set -e
 
 env
 
-pushd $CODEBUILD_SRC_DIR/samples/node/pub_sub
+pushd $CODEBUILD_SRC_DIR/samples/node/basic_connect
 
 ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "unit-test/endpoint" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
 
 npm install --unsafe-perm
 
-echo "Mqtt Direct test"
+echo "Connect Basic (Direct) test"
 node dist/index.js --endpoint $ENDPOINT --key /tmp/privatekey.pem --cert /tmp/certificate.pem
 
-echo "Websocket test"
+popd
+
+pushd $CODEBUILD_SRC_DIR/samples/node/websocket_connect
+
+echo "Connect Websocket test"
 node dist/index.js --endpoint $ENDPOINT --use_websocket --signing_region us-east-1
 
 popd
