b9cb6f0107931dfd6cb10e893437fa5cffd3e8d1
Michael Graeb <graebm@amazon.com>
2022-04-22 14:03:11 -0700

    Autodocs (#242)
    Automated update the API documentation published to https://aws.github.io/aws-iot-device-sdk-js-v2/
    
    **Issue:**
    - Docs weren't updated unless someone remembered to do it manually.
    - Manually updating docs whenever you update code pollutes the diff, making it hard to review
    - Having HTML docs in `main` branch can cause issues.
       - Bloats size.
       - Generated docs might contain licenses we don't want to be redistributing.
    
    **Description of changes:**
    - Docs are kept in `docs` branch now
        - New `docs.yml` Github Workflow updates the `docs` branch whenever the `main` branch changes.
        - New `check-docs` job in `ci.yml` checks pull-requests for anything that would break docs.
        - delete the `docs/` folder in `main` and use `.gitignore` to prevent someone from accidentally resurrecting it.

--- make-docs.sh
+++ make-docs.sh
@@ -2,11 +2,20 @@
 
 set -ex
 
+# clean
+rm -rf docs/
+
+# grab local copy of aws-crt-nodejs so we can build its docs into our own
 if [ ! -d build/docs/aws-crt-nodejs ]; then
     mkdir -p build/docs
     git clone --single-branch https://github.com/awslabs/aws-crt-nodejs.git build/docs/aws-crt-nodejs
 fi
 
+CRT_VERSION=`npm view aws-crt version`
+pushd build/docs/aws-crt-nodejs
+git fetch
+git checkout "v$CRT_VERSION"
+popd
+
+# build docs
 npx typedoc --options typedoc.json
-
-
