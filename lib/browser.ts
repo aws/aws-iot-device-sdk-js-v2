b9074ad8eb2aeda8c3c37d1a5470b55f3ad00728
TwistedTwigleg <ncbeard@amazon.com>
2022-05-26 15:50:49 -0400

    Added custom authorizer sample (#246)
    Adds custom authorizer support and samples!
    
    Commit log:
    * Added custom authorizer sample
    * Renamed codebuild to proper name
    * Added custom authorizer connect sample for the browser
    * Added custom authorizer documentation to the README
    * Added missing header links to the samples in the README
    * Fixed README headers not being correct for custom authorizer sample
    * Updated to use latest CRT release
    * Rebuilt using an older version of node so the package-lock version is correct
    * Regenerated package-lock.json with clean node run

---
+++ lib/browser.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+/**
+ * Browser entry point for AWS IoT SDK.
+ * @packageDocumentation
+ * @module aws-iot-device-sdk
+ * @preferred
+ */
+
+import { mqtt, http, io, iot } from 'aws-crt/dist.browser/browser';
+
+export {
+    mqtt,
+    http,
+    io,
+    iot
+}
