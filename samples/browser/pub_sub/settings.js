af680695137a650475718ace77e3f71d9d24834e
Noah Beard <ncbeard@amazon.com>
2022-07-13 14:00:07 -0400

    remove static credential settings from settings.js in pubsub browser sample

--- samples/browser/pub_sub/settings.js
+++ samples/browser/pub_sub/settings.js
@@ -1,6 +1,3 @@
 export const AWS_REGION = "<your region>";
 export const AWS_IOT_ENDPOINT = "<your endpoint>";
 export const AWS_COGNITO_IDENTITY_POOL_ID = "<your cognito identity pool id>";
-export const AWS_STATIC_ACCESS_KEY = "<your static access key>";
-export const AWS_STATIC_SECRET_ACCESS_KEY = "<your static secret access key>";
-export const AWS_STATIC_ACCESS_TOKEN = "Optional: <your static access token>";
