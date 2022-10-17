d051c9b385741cd4850e420be10c7d87808ba5d3
Noah Beard <ncbeard@amazon.com>
2022-10-17 09:42:14 -0400

    Initial commit for new CD workflow

---
+++ codebuild/new_cd/test-publish.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -ex
+
+if [ ! -f VERSION ]; then
+    echo "No VERSION file found! Cannot make release!"
+    exit 1
+else
+    echo "VERSION file found..."
+fi
+VERSION=$(cat VERSION)
+
+# Make sure the version variable is populated
+if [ -z "${VERSION}" ]; then
+    echo "VERSION file is empty!"
+    exit 1
+else
+    echo "VERSION file contains: ${VERSION}"
+fi
+
+# Make sure the version follows the correct format: major.minor.patch
+LENGTH_CHECK="${VERSION//[^.]}"
+if [ ${#LENGTH_CHECK} != 2 ]; then
+    echo "VERSION file contains invalid version (not in format major.minor.patch)"
+    exit 1
+fi
+# Use RegX to ensure it only contains numbers and periods
+REGX_CHECK='^([0-9]+\.){0,2}(\*|[0-9]+)$'
+if [[ $VERSION =~ $REGX_CHECK ]]; then
+    echo "VERSION file contains valid version"
+else
+    echo "VERSION file contains invalid version (RegX validator failed)"
+    exit 1
+fi
+
+PUBLISHED_TAG_VERSION=`npm show aws-iot-device-sdk-v2 version`
+if [ "$PUBLISHED_TAG_VERSION" == "$VERSION" ]; then
+    echo "$VERSION found in npm. Testing release..."
+
+    # TODO - run the PubSub sample?
+    # curl https://www.amazontrust.com/repository/AmazonRootCA1.pem --output /tmp/AmazonRootCA1.pem
+    # cert=$(aws secretsmanager get-secret-value --secret-id "unit-test/certificate" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo "$cert" > /tmp/certificate.pem
+    # key=$(aws secretsmanager get-secret-value --secret-id "unit-test/privatekey" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo "$key" > /tmp/privatekey.pem
+    # endpoint=$(aws secretsmanager get-secret-value --secret-id "unit-test/endpoint" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
+
+    # cd samples/node/pub_sub
+    # npm install
+    # npm run tsc
+    # node dist/index.js --ca_file /tmp/AmazonRootCA1.pem --cert /tmp/certificate.pem --key /tmp/privatekey.pem --endpoint $endpoint --verbosity info
+
+    exit 0
+fi
+
+exit 1
