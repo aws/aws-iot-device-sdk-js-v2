822bf0f03bf36ef7a23367a2395498a34f830c81
Zhihui Xia <zhvxia@amazon.com>
2023-06-07 15:19:53 -0700

    EOF and copyright

--- samples/node/pub_sub_electron_node/Main.ts
+++ samples/node/pub_sub_electron_node/Main.ts
@@ -1,3 +1,7 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
 const { app, BrowserWindow} = require('electron')
 const path = require("path")
 
