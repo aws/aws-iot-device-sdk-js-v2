5174f5bd54545ddd273b7413ce614771629bbb16
Igor Abdrakhimov <igorabd@amazon.com>
2023-11-24 10:37:58 -0800

    Provide config file as argument

--- servicetests/test_cases/test_fleet_provisioning.py
+++ servicetests/test_cases/test_fleet_provisioning.py
@@ -13,27 +13,22 @@ def main():
     argument_parser = argparse.ArgumentParser(
         description="Run Fleet Provisioning test in CI")
     argument_parser.add_argument(
-        "--input-uuid", required=False, help="UUID for thing name. UUID will be generated if this option is omit")
+        "--config-file", required=True,
+        help="JSON file providing command-line arguments to a test")
     argument_parser.add_argument(
         "--thing-name-prefix", required=False, default="",
         help="Prefix for a thing name, should be the same that Fleet Provisioning template uses")
     argument_parser.add_argument(
         "--region", required=False, default="us-east-1", help="The name of the region to use")
     argument_parser.add_argument(
-        "--mqtt-version", required=True, choices=[3, 5], type=int, help="MQTT protocol version to use")
-    argument_parser.add_argument(
-        "--use-csr", required=False, default=False, action='store_true', help="Create certificate from CSR")
+        "--input-uuid", required=False, help="UUID for thing name. UUID will be generated if this option is omit")
     parsed_commands = argument_parser.parse_args()
 
-    current_path = os.path.dirname(os.path.realpath(__file__))
-    cfg_file_mqtt_version = "mqtt3_" if parsed_commands.mqtt_version == 3 else "mqtt5_"
-    cfg_file_csr = "with_csr_" if parsed_commands.use_csr else ""
-    cfg_file = os.path.join(current_path, cfg_file_mqtt_version + "fleet_provisioning_" + cfg_file_csr + "cfg.json")
     input_uuid = parsed_commands.input_uuid if parsed_commands.input_uuid else str(uuid.uuid4())
 
     # Perform fleet provisioning. If it's successful, a newly created thing should appear.
     try:
-        test_result = run_in_ci.setup_and_launch(cfg_file, input_uuid)
+        test_result = run_in_ci.setup_and_launch(parsed_commands.config_file, input_uuid)
     except Exception as e:
         print(f"ERROR: Failed to execute Fleet Provisioning test: {e}")
         test_result = -1
