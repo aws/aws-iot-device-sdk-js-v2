3c4322f0309e80ab93abd4109b687296a456a6ad
Igor Abdrakhimov <igorabd@amazon.com>
2024-06-03 18:17:06 -0700

    Work, kind of

---
+++ test/greengrass/ipc/copy_files.sh
@@ -0,0 +1,7 @@
+cp ../../../samples/node/gg_ipc/artifacts/com.amazon.RpcTest/1.0.0/index.ts .
+cp ../../../utils/run_in_ci.py .
+cp ../../../.github/workflows/ci_run_greengrass_ipc_cfg.json .
+
+npm install @types/ws@8.5.4
+npm install --install-links
+npm pack
