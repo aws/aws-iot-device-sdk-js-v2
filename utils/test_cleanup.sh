772817296bdc770a67676ab1779d71f0931065ed
Bret Ambrose <bambrose@amazon.com>
2025-03-07 13:49:58 -0800

    Update cleanup

--- utils/test_cleanup.sh
+++ utils/test_cleanup.sh
@@ -2,8 +2,6 @@
 
 echo "Undoing environment variables"
 unset $(grep -v '^#' ${PWD}/environment_files.txt | xargs | cut -d "=" -f 1)
-unset AWS_TEST_MQTT5_CERTIFICATE_FILE
-unset AWS_TEST_MQTT5_KEY_FILE
 unset AWS_TEST_MQTT5_IOT_CERTIFICATE_PATH
 unset AWS_TEST_MQTT5_IOT_KEY_PATH
 unset AWS_TEST_IOT_CORE_PROVISIONING_CERTIFICATE_PATH
@@ -12,8 +10,6 @@ unset AWS_TEST_IOT_CORE_PROVISIONING_CSR_PATH
 
 echo "Cleaning up resources..."
 rm "${PWD}/environment_files.txt"
-rm "${PWD}/crt_certificate.pem"
-rm "${PWD}/crt_privatekey.pem"
 rm "${PWD}/iot_certificate.pem"
 rm "${PWD}/iot_privatekey.pem"
 rm "${PWD}/provision_certificate.pem"
