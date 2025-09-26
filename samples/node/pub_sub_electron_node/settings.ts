634cd6e22fdba41bc1873b688e658066ecb988c1
Vera Xia <zhvxia@amazon.com>
2025-09-26 13:45:08 -0700

    wip add ci file validation

--- samples/node/pub_sub_electron_node/settings.ts
+++
@@ -1,4 +0,0 @@
-export const key_file_path = "<private key file path>";
-export const cert_file_path = "<certificate file path>";
-export const endpoint = "<endpoint>";
-export const region = "<region>" ;
